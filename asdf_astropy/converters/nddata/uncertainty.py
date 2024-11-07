1cd0ce6d2f44c3e375b0ed3a0d37ca29bfe057de
Brett <brettgraham@gmail.com>
2024-11-07 10:29:22 -0500

    na

--- asdf_astropy/converters/nddata/uncertainty.py
+++ asdf_astropy/converters/nddata/uncertainty.py
@@ -1,45 +1,21 @@
-class _UncertaintyBaseConverter:
+class UncertaintyBaseConverter(Converter):
+    tags = ("tag:astropy.org:astropy/nddata/uncertainty-*",)
+    types = (
+        "astropy.nddata.nduncertainty.StdDevUncertainty",
+        "astropy.nddata.nduncertainty.UnknownUncertainty",
+        "astropy.nddata.nduncertainty.VarianceUncertainty",
+    )
+
     def from_yaml_tree(self, node, tag, ctx):
-        return (node["array"], node.get("unit"))
+        import astropy.nddata
+        return getattr(astropy.nddata, node["class"])(node["array"], node.get("unit"))
 
     def to_yaml_tree(self, nddata_uncertainty, tag, ctx):
         node = {}
 
+        node["class"] = nddata_uncertainty.__class__.__name__
         node["array"] = nddata_uncertainty.array
         if nddata_uncertainty.unit is not None:
             node["unit"] = nddata_uncertainty.unit
 
         return node
-
-
-class StdDevUncertaintyConverter(_UncertaintyBaseConverter):
-    tags = ("tag:astropy.org:astropy/nddata/stddevuncertainty-*",)
-    types = ("astropy.nddata.nduncertainty.StdDevUncertainty",)
-
-    def from_yaml_tree(self, node, tag, ctx):
-        from astropy.nddata import StdDevUncertainty
-
-        array, unit = super().from_yaml_tree(node, tag, ctx)
-        return StdDevUncertainty(array=array, unit=unit)
-
-
-class UnknownUncertaintyConverter(_UncertaintyBaseConverter):
-    tags = ("tag:astropy.org:astropy/nddata/unknownuncertainty-*",)
-    types = ("astropy.nddata.nduncertainty.UnknownUncertainty",)
-
-    def from_yaml_tree(self, node, tag, ctx):
-        from astropy.nddata import UnknownUncertainty
-
-        array, unit = super().from_yaml_tree(node, tag, ctx)
-        return UnknownUncertainty(array=array, unit=unit)
-
-
-class VarianceUncertaintyConverter(_UncertaintyBaseConverter):
-    tags = ("tag:astropy.org:astropy/nddata/varianceuncertainty-*",)
-    types = ("astropy.nddata.nduncertainty.VarianceUncertainty",)
-
-    def from_yaml_tree(self, node, tag, ctx):
-        from astropy.nddata import VarianceUncertainty
-
-        array, unit = super().from_yaml_tree(node, tag, ctx)
-        return VarianceUncertainty(array=array, unit=unit)
