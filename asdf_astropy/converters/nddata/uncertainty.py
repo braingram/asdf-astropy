3e0e326a7df965c08f6e06118b908777cd916354
Brett <brettgraham@gmail.com>
2024-11-08 10:31:09 -0500

    na

--- asdf_astropy/converters/nddata/uncertainty.py
+++ asdf_astropy/converters/nddata/uncertainty.py
@@ -1,4 +1,4 @@
-class UncertaintyBaseConverter(Converter):
+class UncertaintyConverter:
     tags = ("tag:astropy.org:astropy/nddata/uncertainty-*",)
     types = (
         "astropy.nddata.nduncertainty.StdDevUncertainty",
@@ -6,17 +6,30 @@ class UncertaintyBaseConverter(Converter):
         "astropy.nddata.nduncertainty.VarianceUncertainty",
     )
 
+    # Mapping of uncertainty class name (attribute of astropy.nddata)
+    # and code "name" stored in the data file.
+    # This will need to be separately versioned if the schema is updated.
+    _class_name_to_code = {
+        "StdDevUncertainty": "stddev",
+        "UnknownUncertainty": "unknown",
+        "VarianceUncertainty": "variance",
+    }
+    _class_code_to_name = {v: k for k, v in _class_name_to_code.items()}
+
     def from_yaml_tree(self, node, tag, ctx):
         import astropy.nddata
 
-        return getattr(astropy.nddata, node["class"])(node["array"], node.get("unit"))
+        class_name = self._class_code_to_name[node["name"]]
 
-    def to_yaml_tree(self, nddata_uncertainty, tag, ctx):
-        node = {}
+        return getattr(astropy.nddata, class_name)(node["array"], unit=node.get("unit"))
 
-        node["class"] = nddata_uncertainty.__class__.__name__
-        node["array"] = nddata_uncertainty.array
-        if nddata_uncertainty.unit is not None:
-            node["unit"] = nddata_uncertainty.unit
+    def to_yaml_tree(self, obj, tag, ctx):
+        node = {
+            "name": self._class_name_to_code[obj.__class__.__name__],
+            "array": obj.array,
+        }
+
+        if obj.unit is not None:
+            node["unit"] = obj.unit
 
         return node
